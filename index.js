50f288db473786af7ace7f6933be0dc094b9a8ef
sthtnr <sthtnr@gmail.com>
2020-01-09 20:16:17 +0900

    remove morgan middleware

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
-morgan.token('content', (req, res) => {
-  return JSON.stringify(req.body)
-})
-app.use(
-  morgan(
-    ':method :url :status :res[content-length] - :response-time ms :content'
-  )
-)
-
 let persons = [
   {
     name: 'Arto Hellas',
