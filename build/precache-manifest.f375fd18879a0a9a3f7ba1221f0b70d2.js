d2277217fbff09ec6419d56d7820b0499d544b6e
sthtnr <sthtnr@gmail.com>
2020-01-09 20:44:38 +0900

    deploy frontend code

---
+++ build/precache-manifest.f375fd18879a0a9a3f7ba1221f0b70d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cf044b4d9d007c6ad96119aa7038f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3bc9b38c874faa346d",
+    "url": "/static/css/main.386ab613.chunk.css"
+  },
+  {
+    "revision": "32fc97efe79707c21ee7",
+    "url": "/static/js/2.ef9ac6b4.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.ef9ac6b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa3bc9b38c874faa346d",
+    "url": "/static/js/main.3f762dc1.chunk.js"
+  },
+  {
+    "revision": "31bfc067a06b18f0c566",
+    "url": "/static/js/runtime-main.bbc88e4f.js"
+  }
+]);
